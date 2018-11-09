98720d272839a910378fa25759aae184116b21cf
Celia Macas <celiamacas24@gmail.com>
2018-11-09 10:46:57 -0500

    Leccion

---
+++ ejercicio1/mipaquete/empleado.py
@@ -0,0 +1,17 @@
+class empleado (object)
+def --int-- (self)
+self. nombre =" "
+self. apellido =" "
+self. cedula= " "
+self. comision.fija=0
+
+def agregar_comision_fija(self.comision)
+self.comision_fija =comision
+
+def obtener_comision_fija(self):
+	return self.comision_fija
+
+def presentar datos(self)
+cadena = "Informacion de %s %s\n\t (cedula %s"% (self.obtener_nombre (),
+                                                 self apellido,
+                                                 self obtener_cedula())
---
+++ ejercicio1/run.py
@@ -0,0 +1,6 @@
+#Crear empleado
+e= empleado()
+e agregar_nombre ("Lius")
+e agregar_apellido ("Benitez")
+e agregar_cedula ("")
+print (e presentar_datos ())
