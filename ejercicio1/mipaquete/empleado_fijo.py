0df029fba44909f61067459f94c8f4b50561f52a
Celia Macas <celiamacas24@gmail.com>
2018-11-09 10:44:00 -0500

    Leccion Python

---
+++ ejercicio1/mipaquete/empleado_fijo.py
@@ -0,0 +1,8 @@
+class empleado_fijo (object)
+def --int --(self)
+ self. sueldo.fijo = " "
+ self. descuento.seguro = " "
+
+ def calcular_sueldo_final (self.sueldo_fijo):
+  self.sueldo_fijo = sueldo 
+ 	
